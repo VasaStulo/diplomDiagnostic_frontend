a18c8f219f3671c6dba349a899579a5ff4b5b877
vasilina <vasilinastulova@gmail.com>
2023-02-12 13:11:02 +0300

    add enterComponent + window of auf

--- src/components/EnterComponent.vue
+++ src/components/EnterComponent.vue
@@ -1,5 +1,9 @@
 <template>
-  <h1>ENTER </h1>
+    <v-card
+        class="mx-auto"
+    >
+
+    </v-card>
 </template>
 
 <script>
@@ -7,3 +11,15 @@
     name: 'EnterComponent',
   }
 </script>
+
+<style scoped lang="scss">
+.v-card{
+  height: 500px;
+  width: 1100px;
+  border-radius: 20px;
+  margin-top: 35px;
+  margin-bottom:45px;
+  box-shadow: -4px 4px 23px rgba(0, 0, 0, 0.20) !important;
+}
+
+</style>
